ab34ec143ef0c58b1ed1172c2eff8d6f24936ffd
Tony Dias <tonydiasrodriguess@gmail.com>
2026-02-27 21:45:53 +0100

    Implement echo

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LINE 200
+#define MAX_BASH_LINE 200
+#define MAX_ARG_LINE 200
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -9,13 +10,22 @@ int main(int argc, char *argv[]) {
 
   while (1) {
     printf("$ ");
-    char input[MAX_LINE];
+
+    char input[MAX_BASH_LINE];
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = 0;
-    if (strcmp(input, "exit") == 0) {
+
+    char *commande, *arg;
+    commande = strtok(input, " ");
+    arg = strtok(NULL, "\n");
+
+    if (strcmp(commande, "exit") == 0) {
       break;
+    } else if (strcmp(commande, "echo") == 0) {
+      printf("%s\n", arg);
+    } else {
+      printf("%s: command not found\n", input);
     }
-    printf("%s: command not found\n", input);
   }
 
   return 0;
