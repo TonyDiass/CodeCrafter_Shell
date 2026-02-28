9c9e7aff4264413537c522d3baaf47264a5d1354
Tony Dias <tonydiasrodriguess@gmail.com>
2026-02-28 02:25:36 +0100

    Run a program

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define MAX_BASH_LINE 200
-#define MAX_ARG_LINE 200
 
-char *path_ok(char *command) {
+char *find_in(char *command) {
   char *env_path = getenv("PATH");
   if (!env_path || !command)
     return NULL;
@@ -29,6 +29,7 @@ char *path_ok(char *command) {
 
   return NULL;
 }
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -40,29 +41,46 @@ int main(int argc, char *argv[]) {
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = 0;
 
-    char *commande, *arg;
-    commande = strtok(input, " ");
-    arg = strtok(NULL, "\n");
+    char **args;
+    int i = 0;
+    char *token = strtok(input, " ");
+    while (token != NULL) {
+      args[i++] = token;
+      token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
 
-    if (strcmp(commande, "exit") == 0) {
+    if (strcmp(args[0], "exit") == 0) {
       break;
-    } else if (strcmp(commande, "echo") == 0) {
-      printf("%s\n", arg);
-    } else if (strcmp(commande, "type") == 0) {
-      if (arg && (strcmp(arg, "echo") == 0 || strcmp(arg, "exit") == 0 ||
-                  strcmp(arg, "type") == 0)) {
-        printf("%s is a shell builtin\n", arg);
-      } else {
-        char *path = path_ok(arg);
-        if (path)
-          printf("%s is %s\n", arg, path);
+    } else if (strcmp(args[0], "echo") == 0) {
+      for (int j = 1; j < i; j++)
+        printf("%s%c", args[j], (j == i - 1) ? '\n' : ' ');
+    } else if (strcmp(args[0], "type") == 0) {
+      if (i < 2)
+        continue;
+      if (strcmp(args[1], "echo") == 0 || strcmp(args[1], "exit") == 0 ||
+          strcmp(args[1], "type") == 0)
+        printf("%s is a shell builtin\n", args[1]);
+      else {
+        char *p = find_in(args[1]);
+        if (p)
+          printf("%s is %s\n", args[1], p);
         else
-          printf("%s: not found\n", arg);
+          printf("%s: not found\n", args[1]);
       }
     } else {
-      printf("%s: command not found\n", commande);
+      char *full_path = find_in(args[0]);
+      if (full_path) {
+        if (fork() == 0) {
+          execv(full_path, args);
+          exit(1);
+        } else {
+          wait(NULL);
+        }
+      } else {
+        printf("%s: command not found\n", args[0]);
+      }
     }
   }
-
   return 0;
 }
