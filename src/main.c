4d83c9c8b923c312f3f55a333f14c6d3864a0627
Tony Dias <tonydiasrodriguess@gmail.com>
2026-03-04 10:31:47 +0100

    Implement single quotes parsing & simplify code structure

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,73 +28,116 @@ char *find_in(char *command) {
     }
   }
   free(to_free);
-
   return NULL;
 }
 
+int parse_input(char *input, char **args) {
+  int arg_count = 0;
+  char buffer[MAX_BASH_LINE];
+  int buf_idx = 0;
+  bool in_single_quotes = false;
+<<<<<<< HEAD
+=======
+  bool in_double_quotes = false;
+>>>>>>> d5f6f1f (codecrafters submit [skip ci])
+
+  for (int i = 0; input[i] != '\0'; i++) {
+    char c = input[i];
+
+<<<<<<< HEAD
+    if (c == '\'') {
+      in_single_quotes = !in_single_quotes;
+    } else if (c == ' ' && !in_single_quotes) {
+=======
+    if (c == '\"' && !in_single_quotes) {
+      in_double_quotes = !in_double_quotes;
+    } else if (c == '\'' && !in_double_quotes) {
+      in_single_quotes = !in_single_quotes;
+    } else if (c == ' ' && !in_single_quotes && !in_double_quotes) {
+>>>>>>> d5f6f1f (codecrafters submit [skip ci])
+      if (buf_idx > 0) {
+        buffer[buf_idx] = '\0';
+        args[arg_count++] = strdup(buffer);
+        buf_idx = 0;
+      }
+    } else {
+      buffer[buf_idx++] = c;
+    }
+  }
+
+  if (buf_idx > 0) {
+    buffer[buf_idx] = '\0';
+    args[arg_count++] = strdup(buffer);
+  }
+  args[arg_count] = NULL;
+  return arg_count;
+}
+
+void execute_command(char **args, int arg_count) {
+  if (strcmp(args[0], "exit") == 0) {
+    exit(0);
+  } else if (strcmp(args[0], "echo") == 0) {
+    for (int j = 1; j < arg_count; j++) {
+      printf("%s%c", args[j], (j == arg_count - 1) ? '\n' : ' ');
+    }
+  } else if (strcmp(args[0], "pwd") == 0) {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+      printf("%s\n", cwd);
+  } else if (strcmp(args[0], "cd") == 0) {
+    char *path =
+        (arg_count > 1 && strcmp(args[1], "~") == 0) ? getenv("HOME") : args[1];
+    if (chdir(path) != 0)
+      printf("cd: %s: No such file or directory\n", args[1]);
+  } else if (strcmp(args[0], "type") == 0) {
+    if (arg_count < 2)
+      return;
+    if (strcmp(args[1], "echo") == 0 || strcmp(args[1], "exit") == 0 ||
+        strcmp(args[1], "type") == 0 || strcmp(args[1], "pwd") == 0 ||
+        strcmp(args[1], "cd") == 0)
+      printf("%s is a shell builtin\n", args[1]);
+    else {
+      char *p = find_in(args[1]);
+      if (p)
+        printf("%s is %s\n", args[1], p);
+      else
+        printf("%s: not found\n", args[1]);
+    }
+  } else {
+    char *full_path = find_in(args[0]);
+    if (full_path) {
+      if (fork() == 0) {
+        execv(full_path, args);
+        exit(1);
+      } else {
+        wait(NULL);
+      }
+    } else {
+      printf("%s: command not found\n", args[0]);
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
-  // Flush after every printf
   setbuf(stdout, NULL);
 
   while (1) {
     printf("$ ");
-
     char input[MAX_BASH_LINE];
-    fgets(input, sizeof(input), stdin);
+    if (!fgets(input, sizeof(input), stdin))
+      break;
     input[strcspn(input, "\n")] = 0;
 
     char *args[MAX_ARG];
-    int i = 0;
-    char *token = strtok(input, " ");
-    while (token != NULL) {
-      args[i++] = token;
-      token = strtok(NULL, " ");
-    }
-    args[i] = NULL;
+    int arg_count = parse_input(input, args);
 
-    if (i == 0)
+    if (arg_count == 0)
       continue;
 
-    if (strcmp(args[0], "exit") == 0) {
-      break;
-    } else if (strcmp(args[0], "echo") == 0) {
-      for (int j = 1; j < i; j++)
-        printf("%s%c", args[j], (j == i - 1) ? '\n' : ' ');
-    } else if (strcmp(args[0], "pwd") == 0) {
-      printf("%s\n", getcwd(NULL, 0));
-    } else if (strcmp(args[0], "cd") == 0) {
-      if (strcmp(args[1], "~") == 0) {
-        chdir(getenv("HOME"));
-      } else if (chdir(args[1]) != 0) {
-        printf("cd: %s: No such file or directory\n", args[1]);
-      }
-    } else if (strcmp(args[0], "type") == 0) {
-      if (i < 2)
-        continue;
-      if (strcmp(args[1], "echo") == 0 || strcmp(args[1], "exit") == 0 ||
-          strcmp(args[1], "type") == 0 || strcmp(args[1], "pwd") == 0 ||
-          strcmp(args[0], "cd") == 0)
-        printf("%s is a shell builtin\n", args[1]);
-      else {
-        char *p = find_in(args[1]);
-        if (p)
-          printf("%s is %s\n", args[1], p);
-        else
-          printf("%s: not found\n", args[1]);
-      }
-    } else {
-      char *full_path = find_in(args[0]);
-      if (full_path) {
-        if (fork() == 0) {
-          execv(full_path, args);
-          exit(1);
-        } else {
-          wait(NULL);
-        }
-      } else {
-        printf("%s: command not found\n", args[0]);
-      }
-    }
+    execute_command(args, arg_count);
+
+    for (int k = 0; k < arg_count; k++)
+      free(args[k]);
   }
   return 0;
 }
